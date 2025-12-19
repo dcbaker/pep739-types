c393e79f7b1d487df2faf2d84c5c96b21751ed69
Dylan Baker <dylan@pnwbakers.com>
2025-12-19 15:52:30 -0800

    Fix typo 379 -> 739

--- pep739/__init__.pyi
+++ pep739/__init__.pyi
@@ -6,7 +6,7 @@ from typing_extensions import TypeAlias
 from . import v1_0
 
 # This is a union of every major version 1 revision of the build-details.json
-PEP379_v1: TypeAlias = v1_0.PEP739
+PEP739_v1: TypeAlias = v1_0.PEP739
 
 # This is a union of every possible version of the build-details.json
-PEP379: TypeAlias = PEP379_v1
+PEP739: TypeAlias = PEP739_v1
