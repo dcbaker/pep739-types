06b7cd5faa3024fc899b2fd5b1eac829b68a8195
Dylan Baker <dylan@pnwbakers.com>
2025-12-19 14:58:27 -0800

    Move to a versioned API
    This allows for specific versions to be more readily selected through
    the versioned module names, and to more easily work with future
    versions, without an API break.

--- pep739/__init__.pyi
+++ pep739/__init__.pyi
@@ -1,76 +1,12 @@
 # SPDX-License-Identifier: MIT
 # Copyright © 2025 Dylan Baker
 
-from typing import Literal, TypedDict
+from typing_extensions import TypeAlias
 
-from typing_extensions import NotRequired
+from . import v1_0
 
+# This is a union of every major version 1 revision of the build-details.json
+PEP379_v1: TypeAlias = v1_0.PEP739
 
-class VersionInfo(TypedDict):
-
-    major: int
-    minor: int
-    micro: int
-    serial: int
-    releaselevel: Literal['alpha', 'beta', 'candidate', 'final']
-
-
-class Language(TypedDict):
-
-    version: str
-    version_info: NotRequired[VersionInfo]
-
-
-class Implementation(TypedDict):
-
-    # TODO: this allows extra properties, which currently cannot be modeled
-
-    name: str
-    version: VersionInfo
-
-
-class ABI(TypedDict):
-
-    flags: list[str]
-    extension_suffix: NotRequired[str]
-    stable_abi_suffix: NotRequired[str]
-
-
-class Suffixes(TypedDict):
-
-    # TODO: this allows extra properties, which currently cannot be modeled
-
-    source: list[str]
-    bytecode: list[str]
-    debug_bytecode: list[str]
-    optimized_bytecode: list[str]
-    extension: list[str]
-
-
-class LibPython(TypedDict, total=False):
-
-    dynamic: str
-    dynamic_stableabi: str
-    static: str
-    link_extensions: bool
-
-
-class CAPI(TypedDict):
-
-    headers: str
-    pkgconfig_path: NotRequired[str]
-
-
-class PEP739(TypedDict):
-
-    schema_version: str
-    base_prefix: str
-    base_interpreter: NotRequired[str]
-    platform: str
-    language: Language
-    implementation: Implementation
-    abi: NotRequired[ABI]
-    suffixes: NotRequired[Suffixes]  # TODO
-    libpython: NotRequired[LibPython]
-    c_api: NotRequired[CAPI]
-    arbitrary_data: NotRequired[dict[str, object]]
+# This is a union of every possible version of the build-details.json
+PEP379: TypeAlias = PEP379_v1
---
+++ pep739/v1_0.pyi
@@ -0,0 +1,78 @@
+# SPDX-License-Identifier: MIT
+# Copyright © 2025 Dylan Baker
+
+"""Revision 1.0 of the build-details.json."""
+
+from typing import Literal, TypedDict
+
+from typing_extensions import NotRequired
+
+
+class VersionInfo(TypedDict):
+
+    major: int
+    minor: int
+    micro: int
+    serial: int
+    releaselevel: Literal['alpha', 'beta', 'candidate', 'final']
+
+
+class Language(TypedDict):
+
+    version: str
+    version_info: NotRequired[VersionInfo]
+
+
+class Implementation(TypedDict):
+
+    # TODO: this allows extra properties, which currently cannot be modeled
+
+    name: str
+    version: VersionInfo
+
+
+class ABI(TypedDict):
+
+    flags: list[str]
+    extension_suffix: NotRequired[str]
+    stable_abi_suffix: NotRequired[str]
+
+
+class Suffixes(TypedDict):
+
+    # TODO: this allows extra properties, which currently cannot be modeled
+
+    source: list[str]
+    bytecode: list[str]
+    debug_bytecode: list[str]
+    optimized_bytecode: list[str]
+    extension: list[str]
+
+
+class LibPython(TypedDict, total=False):
+
+    dynamic: str
+    dynamic_stableabi: str
+    static: str
+    link_extensions: bool
+
+
+class CAPI(TypedDict):
+
+    headers: str
+    pkgconfig_path: NotRequired[str]
+
+
+class PEP739(TypedDict):
+
+    schema_version: Literal['1.0']
+    base_prefix: str
+    base_interpreter: NotRequired[str]
+    platform: str
+    language: Language
+    implementation: Implementation
+    abi: NotRequired[ABI]
+    suffixes: NotRequired[Suffixes]  # TODO
+    libpython: NotRequired[LibPython]
+    c_api: NotRequired[CAPI]
+    arbitrary_data: NotRequired[dict[str, object]]
